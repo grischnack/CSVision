a94b99cbac80e926d153f6ce5d8f655cee695f86
Gergely Csaki <gyula42@gmail.com>
2017-03-18 23:15:19 +0100

    camera orbiting modified

--- cspoint3d.cpp
+++ cspoint3d.cpp
@@ -6,13 +6,13 @@ CsPoint3D::CsPoint3D(float xx, float yy, float zz){
     z = zz;
     dist =sqrt(pow( xx, 2)+pow(yy,2)+pow((zz),2));
     inclination = acos(zz/dist);
-    azimut =atan(yy/xx);
+    azimut = atan2(yy,xx);
 }
 
 CsPoint3D::CsPoint3D(bool noth, float r, float inc, float az){
-    x = r * cos(inc) * cos(az);
-    y = r * sin(inc) * cos(az);
-    z = r * cos(inc);
+    x = r * sin(inc) * cos(az);
+    y = r * sin(inc) * sin(az);
+    z = r * cos(az);
     dist = r;
     inclination = inc;
     azimut = az;
